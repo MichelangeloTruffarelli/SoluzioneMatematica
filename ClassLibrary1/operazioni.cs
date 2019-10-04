dd9067da4f2646fac5f706a05f40d0b70019a911
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-04 13:10:48 +0200

    Add project files.

---
+++ ClassLibrary1/operazioni.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Operazioni
+    {
+
+        public static int Somma(int a, int b)
+        {
+            int s = a + b;
+            return s;
+        }
+        public static int Sottrazione(int a, int b)
+        {
+            int d = a - b;
+            return d;
+        }
+        public static double Divisione(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new Exception("b == 0");
+            }
+            double f = a / b;
+            return f;
+        }
+        public static double Moltiplicazione(double a, double b)
+        {
+            double m = a * b;
+            return m;
+        }
+        public static double Potenza(double a, double b)
+        {
+            double p = 1;
+            int c = 0;
+            do
+            {
+                p = p * a;
+                c++;
+            } while (c < b);
+            return p;
+        }
+        public static int Massimo(int a, int b)
+        {
+            if (a > b)
+            {
+                int max;
+                max = a;
+                return max;
+            }
+            else
+            {
+                int max;
+                max = b;
+                return max;
+            }
+        }
+        public static int Minimo(int a, int b)
+        {
+            if (a < b)
+            {
+                int min;
+                min = a;
+                return min;
+            }
+            else
+            {
+                int min;
+                min = b;
+                return min;
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci due numeri");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int Somma = Operazioni.Somma(a, b);
+            Console.WriteLine("la somma è:");
+            Console.WriteLine(Somma);
+            int Sottrazione = Operazioni.Sottrazione(a, b);
+            Console.WriteLine("la sottrzione è:");
+            Console.WriteLine(Sottrazione);
+            double Divisione = Operazioni.Divisione(a, b);
+            Console.WriteLine("la divisione è:");
+            Console.WriteLine(Divisione);
+            double Moltiplicazione = Operazioni.Moltiplicazione(a, b);
+            Console.WriteLine("la moltiplicazione è:");
+            Console.WriteLine(Moltiplicazione);
+            double Potenza = Operazioni.Potenza(a, b);
+            Console.WriteLine("la potenza è:");
+            Console.WriteLine(Potenza);
+            int Massimo = Operazioni.Massimo(a, b);
+            Console.WriteLine("il max è:");
+            Console.WriteLine(Massimo);
+            int Minimo = Operazioni.Minimo(a, b);
+            Console.WriteLine("il min è:");
+            Console.WriteLine(Minimo);
+            Console.ReadLine();
+
+        }
+    }
+}
